802d9e9fe83498b629d233ee8be5fd3a4522e67a
usama hamed <osama.hamed@roomle.com>
2025-02-11 09:47:03 +0100

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Administrator on 11.02.25.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ memorizeTests/memorizeTests.swift
@@ -0,0 +1,16 @@
+//
+//  memorizeTests.swift
+//  memorizeTests
+//
+//  Created by Administrator on 11.02.25.
+//
+
+import Testing
+
+struct memorizeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
